f4ee6a9dd9628dbd599e9b4cf4bffd7a13172641
Nick Rodriguez <nickrodriguez@Nicks-MacBook-Air.local>
2021-03-14 19:48:19 -0500

    Summary: Added username on signup through query params. Description: 1. Want to input username into DB on socket creation. 2. Modified connectHandler. 3. Modified html to include a text box for username and to append the params.

--- connect_handler.py
+++ connect_handler.py
@@ -1,12 +1,16 @@
 import json
 import boto3
 import os
+import urllib3
 
 dynamodb = boto3.client('dynamodb')
 
 
 def handle(event, context):
+    print(f'## EVENT: {event} ##')
+    print(f'## CONTEXT: {context} ##')
     connectionId = event['requestContext']['connectionId']
-    item = {'connectionId': {'S': connectionId}}
+    username = event['queryStringParameters'].get('username', '')
+    item = {'connectionId': {'S': connectionId}, 'username': {'S': username}}
     dynamodb.put_item(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME'], Item=item)
     return {}
