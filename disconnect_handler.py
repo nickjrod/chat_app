ef4907cc92bea64142af1965f203ce6b9f2f916e
Nick Rodriguez <nickrodriguez@Nicks-MacBook-Air.local>
2021-03-16 22:56:27 -0500

    Summary: Added connect and disconnect alerts.
    Description:
    1. Modified connect_handler to notify all connections of a new connection.
    2. Modified disconnect_handler to notify all connections of a disconnection.
    
    Notes:
    We will need a way to purge all orphaned connections.
    We will need a way to "cache" a connection for easy reconnect.

--- disconnect_handler.py
+++ disconnect_handler.py
@@ -5,12 +5,39 @@ import os
 dynamodb = boto3.client('dynamodb')
 
 
+def broadcast_disconnection(event, username, disconnecting_id):
+    """Broadcast that the user has disconnected to every connection in the DB. """
+    full_message = f'{username} has disconnected.'
+    paginator = dynamodb.get_paginator('scan')
+
+    connectionIds = []
+
+    apigatewaymanagementapi = boto3.client(
+        'apigatewaymanagementapi',
+        endpoint_url="https://" + event["requestContext"]["domainName"] + "/" + event["requestContext"]["stage"])
+
+    # Retrieve all connectionIds from the database
+    for page in paginator.paginate(TableName=os.environ['SOCKET_CONNECTIONS_TABLE_NAME']):
+        connectionIds.extend(page['Items'])
+    # Emit the recieved message to all the connected devices
+    for connectionIdObj in connectionIds:
+        connectionId = connectionIdObj['connectionId']['S']
+        if connectionId != disconnecting_id:
+            apigatewaymanagementapi.post_to_connection(
+                Data=full_message,
+                ConnectionId=connectionId)
+
+
 def handle(event, context):
-	connectionId = event['requestContext']['connectionId']
-	
-	# Delete connectionId from the databaseNAME
-	table_name = os.environ['SOCKET_CONNECTIONS_TABLE_NAME']
-	key = {'connectionId': {'S': connectionId}}
-	dynamodb.delete_item(TableName=table_name, Key=key)
-	
-	return {}
+    connection_id = event['requestContext']['connectionId']
+    # Delete connectionId from the database
+    table_name = os.environ['SOCKET_CONNECTIONS_TABLE_NAME']
+    item_key = {'connectionId': {'S': connection_id}}
+    item = dynamodb.get_item(TableName=table_name, Key=item_key)
+    username = item['Item']['username']['S']
+
+    dynamodb.delete_item(TableName=table_name, Key=item_key)
+
+    # Alert everyone else in chatroom that user has disconnected.
+    broadcast_disconnection(event, username, connection_id)
+    return {}
